6f394bea9625bf482d52511f1ac29a2afbbfc21b
Lazersmoke <lazersmoke@gmail.com>
2016-09-04 14:23:29 -0500

    Made 10x better

--- src/Data/TypeVector.hs
+++ src/Data/TypeVector.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -13,24 +14,17 @@ import Prelude hiding (head, tail, length, init, last)
 
 data Nat = Zero | Succ Nat
 
-type One = Succ Zero
-type Two = Succ One
-type Three = Succ Two
+type One = 'Succ 'Zero
+type Two = 'Succ One
+type Three = 'Succ Two
 
-type family Plus (n :: Nat) (m :: Nat) :: Nat
-type instance Plus Zero m = m
-type instance Plus (Succ n) m = Succ (Plus n m)
-
-type family Times (n :: Nat) (m :: Nat) :: Nat
-type instance Times Zero m = Zero
-type instance Times (Succ n) m = Plus m (Times n m)
-
-type family LessThan (n :: Nat) :: Nat
-type instance LessThan (Succ n) = n
+type family Plus (n :: Nat) (m :: Nat) where
+  Plus 'Zero n = n
+  Plus ('Succ n) m = 'Succ (Plus n m)
 
 data Vector :: Nat -> * -> * where
-  Nil :: Vector Zero a
-  (:>) :: a -> Vector n a -> Vector (Succ n) a
+  Nil :: Vector 'Zero a
+  (:>) :: a -> Vector n a -> Vector ('Succ n) a
 
 infixr 5 :>
 
@@ -46,14 +40,20 @@ instance Show a => Show (Vector n a) where
 instance Functor (Vector n) where
   fmap f (x :> xs) = f x :> fmap f xs
   fmap _ Nil = Nil
-
+instance Eq a => Eq (Vector n a) where
+  (==) (x :> xs) (y :> ys) = x == y && xs == ys
+  (==) Nil Nil = True
+  (==) _ _ = error "Impossible because both vectors are length n"
+  
 applyVector :: Vector n (a -> b) -> Vector n a -> Vector n b
 applyVector (f :> fs) (x :> xs) = f x :> applyVector fs xs
 applyVector Nil Nil = Nil
+applyVector _ _ = error "Impossible because both vectors are length n"
 
 addVector :: Num a => Vector n a -> Vector n a -> Vector n a
 addVector (a :> veca) (b :> vecb) = a + b :> addVector veca vecb
 addVector Nil Nil = Nil
+addVector _ _ = error "Impossible because both vectors are length n"
 
 fromValue :: a -> Vector One a
 fromValue x = x :> Nil
@@ -65,37 +65,39 @@ toList :: Vector n a -> [a]
 toList Nil = []
 toList (x :> xs) = x : toList xs
 
-uncons :: Vector (Succ n) a -> (a, Vector n a)
+uncons :: Vector ('Succ n) a -> (a, Vector n a)
 uncons (x :> xs) = (x,xs)
 
-head :: Vector (Succ n) a -> a
+head :: Vector ('Succ n) a -> a
 head (x :> _) = x
 
-tail :: Vector (Succ n) a -> Vector n a
+tail :: Vector ('Succ n) a -> Vector n a
 tail (_ :> xs) = xs
 
-last :: Vector (Succ n) a -> a
+last :: Vector ('Succ n) a -> a
 last (x :> Nil) = x
 last (_ :> xs@(_ :> _)) = last xs
 
-init :: Vector (Succ n) a -> Vector n a
+init :: Vector ('Succ n) a -> Vector n a
 init (_ :> Nil) = Nil
 init (x :> xs@(_ :> _)) = x :> init xs
 
 append :: Vector n a -> Vector m a -> Vector (Plus n m) a
-append (a :> veca) vec = a :> append veca vec
-append Nil vec = vec
+append (x :> xs) ys = x :> append xs ys
+append Nil ys = ys
 
 dotProduct :: Num a => Vector n a -> Vector n a -> a
 dotProduct (x :> xs) (y :> ys) = x * y + dotProduct xs ys
 dotProduct Nil Nil = 0
+dotProduct _ _ = error "Impossible because both vectors are length n"
 
 crossProduct :: Num a => Vector Three a -> Vector Three a -> Vector Three a
 crossProduct (a1:>a2:>a3:>Nil) (b1:>b2:>b3:>Nil) =
   (a2 * b3 - a3 * b2) :> (a3 * b1 - a1 * b3) :> (a1 * b2 - a2 * b1) :> Nil
+crossProduct _ _ = error "Impossible because both vectors are length 3"
 
 length :: Num q => Vector n a -> q
-length (x :> xs) = 1 + length xs
+length (_ :> xs) = 1 + length xs
 length Nil = 0
 
 null :: Vector n a -> Bool
--- test/TypeVectorTest.hs
+++ test/TypeVectorTest.hs
@@ -1,7 +1,17 @@
 
 import Test.HUnit
+import Data.TypeVector
 
+main :: IO Counts
 main = runTestTT tests
 
+tests :: Test
 tests = test
-  ["5=5" ~: 5 ~=? 5]
+  ["toList works" ~: [1,2 :: Integer] ~=? toList (1 :> 2 :> Nil) 
+  ,"fromValue works" ~: (5 :> Nil) ~=? fromValue (5 :: Integer)
+  ,"fromPair works" ~: (5 :> 3 :> Nil) ~=? fromPair (5 :: Integer, 3)
+  ,"applyVector works" ~: ("Yay!!!" :> "Hmm..." :> Nil) ~=? fromPair ((++"!!!"),(++"...")) `applyVector` fromPair ("Yay","Hmm")
+  ,"addVector works" ~: (6 :> 9 :> Nil) ~=? fromPair (3,2) `addVector` fromPair (3,7 :: Integer)
+  ,"last works" ~: (4 :: Integer) ~=? Data.TypeVector.last (1 :> 2 :> 3 :> 4 :> Nil)
+  ,"append works" ~: ((1 :: Integer) :> 2 :> 3 :> 4 :> Nil) ~=? (1 :> 2 :> Nil) `append` (3 :> 4 :> Nil)
+  ]
